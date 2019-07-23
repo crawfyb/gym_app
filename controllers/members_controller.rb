8ad149ced7b1996ed023db4f585233662f1d18d7
crawfyb <crawford.brown@outlook.com>
2019-07-23 14:12:09 +0100

    ..

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -9,8 +9,20 @@ get '/members' do
   erb(:"members/index")
 end
 
+# new
+get '/members/new' do
+  erb(:"members/new")
+end
+
 # show
 get '/members/:id' do
   @members = Member.find(params['id'].to_i)
   erb(:"members/show")
+end
+
+# create
+post '/members' do
+  @members = Member.new(params)
+  @members.save()
+  erb(:"members/create")
 end
